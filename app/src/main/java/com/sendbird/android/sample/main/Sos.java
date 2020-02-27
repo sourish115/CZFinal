6c723caa945111bd5f2dcb5d454f8a95354c07c8
mehak <g.mehak26@gmail.com>
2020-02-27 13:47:53 +0530

    Emergency contacts added

--- app/src/main/java/com/sendbird/android/sample/main/Sos.java
+++ app/src/main/java/com/sendbird/android/sample/main/Sos.java
@@ -1,6 +1,6 @@
 package com.sendbird.android.sample.main;
 
-import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.sendbird.android.sample.R;
 
@@ -30,21 +33,19 @@ public class Sos extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    ImageButton call1,call2,call3;
+    EditText num1, num2, num3;
+    String numcall;
+
+
+
     private OnFragmentInteractionListener mListener;
 
     public Sos() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Sos.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static Sos newInstance(String param1, String param2) {
         Sos fragment = new Sos();
         Bundle args = new Bundle();
@@ -61,14 +62,48 @@ public class Sos extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sos, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_sos, container, false);
+        call1=rootView.findViewById(R.id.call1);
+        call2=rootView.findViewById(R.id.call2);
+        call3=rootView.findViewById(R.id.call3);
+        num1 =rootView.findViewById(R.id.num1);
+        num2 =rootView.findViewById(R.id.num2);
+        num3 =rootView.findViewById(R.id.num3);
+
+        call1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Intent.ACTION_CALL);
+                numcall= num1.getText().toString();
+                //numcall = name1.getText().toString();
+                if(numcall.equals(null)){
+                   // i.setData(Uri.parse("tel:998638834")); // default dial number
+                    Toast.makeText(getContext(),"Please enter a number",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getContext(),numcall.toString(),Toast.LENGTH_SHORT).show();
+                    i.setData(Uri.parse("tel:"+numcall));
+                    startActivity(i);
+                }
+              //  startActivity(i);
+            }
+        });
+        return rootView;
     }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -77,33 +112,8 @@ public class Sos extends Fragment {
         }
     }
 
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
